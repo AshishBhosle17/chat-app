89056b3825897ec03536de7d88da7eadb3df62b2
Ashish Bhosle <asbhosle2003@gmail.com>
2024-01-05 08:25:42 -0800

    Added my project

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFD2p22aGQP3aRUMOa24ZcSH9csFcRLRw",
+    authDomain: "chat-app-by-ashish.firebaseapp.com",
+    projectId: "chat-app-by-ashish",
+    storageBucket: "chat-app-by-ashish.appspot.com",
+    messagingSenderId: "945011277586",
+    appId: "1:945011277586:web:f471c4dcc9f353c8693dcb",
+    measurementId: "G-MTNH0Z2SDS"
+  };
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
